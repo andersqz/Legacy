cfcde9624d6c278be9054543bbed820d00c2c76b
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:41:01 -0300

    add pedido interface-repository

---
+++ Legacy.Domain/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Legacy.Domain.Entities;
+
+namespace Legacy.Domain.Interfaces;
+
+public interface IPedidoRepository
+{
+    Task<IEnumerable<Pedido>> SelecionarTodos(int pagina, int tamanhoPagina);
+    Task<Pedido?> SelecionarPorInfo(int empresa, int local, string serie, int data, int doc);
+    Task<int> ContarTodos();
+}
