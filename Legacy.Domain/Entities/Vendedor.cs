be2e078ebce365702ecb3ef1e79d3d1c56769f00
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:20:29 -0300

    add vendedor entity

---
+++ Legacy.Domain/Entities/Vendedor.cs
@@ -0,0 +1,25 @@
+﻿namespace Legacy.Domain.Entities;
+
+public class Vendedor
+{
+    public int EmpresaId { get; set; }
+    public int VendedorId { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public char Tipo { get; set; }
+    public bool CalculaComissao { get; set; }
+    public double PercentualComissao { get; set; }
+    public bool VendedorAtivo { get; set; }
+    public string Endereco { get; set; } = string.Empty;
+    public string CEP { get; set; } = string.Empty;
+    public char TipoPessoa { get; set; }
+    public string CpfCnpj { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public double PercMaximoComissao { get; set; }
+    public double PercMaximoDesc { get; set; }
+
+    public Vendedor()
+    {
+            
+    }
+}
