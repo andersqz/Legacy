9ab1952cb68e5a66f02a807ebf18e17d052dd72e
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-10 08:08:38 -0300

    add empresa dto

---
+++ Legacy.App/Dtos/EmpresaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Legacy.App.Dtos;
+
+public class EmpresaDto
+{
+    public int CodigoEmpresa { get; set; }
+    public string NomeEmpresa { get; set; } = string.Empty;
+    public string TipoEmpresa { get; set; } = string.Empty;
+    public string NomeFantasia { get; set; } = string.Empty;
+}
