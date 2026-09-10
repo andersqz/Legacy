22cc94642d09c30f82d75a0cb390bc7031db811d
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:15:29 -0300

    add empresa entity

---
+++ Legacy.Domain/Entities/Empresa.cs
@@ -0,0 +1,9 @@
+﻿namespace Legacy.Domain.Entities;
+
+public class Empresa
+{
+    public int CodigoEmpresa { get; set; }
+    public string NomeEmpresa { get; set; } = string.Empty;
+    public string TipoEmpresa { get; set; } = string.Empty;
+    public string NomeFantasia { get; set; } = string.Empty;
+}
