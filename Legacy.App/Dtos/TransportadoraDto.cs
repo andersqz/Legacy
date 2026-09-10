2b1f744065df35e8231eaefa9af75c7c49f32df4
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-10 08:15:24 -0300

    add transportadora dto

---
+++ Legacy.App/Dtos/TransportadoraDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Legacy.App.Dtos;
+
+public class TransportadoraDto
+{
+    public int EmpresaId { get; set; }
+    public int TransportadoraId { get; set; }
+    public string Descricao { get; set; }
+    public string Endereco { get; set; }
+    public string CEP { get; set; }
+    public char TipoPessoa { get; set; }    
+    public string CpfCnpj { get; set; }
+    public string InscricaoEstadual { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
