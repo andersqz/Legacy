b87084b633d6186f5c866287a7cfd48c4b30fcf5
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:30:43 -0300

    add cliente interface-repository

---
+++ Legacy.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Legacy.Domain.Interfaces;
+
+public class IClienteRepository
+{
+    Task<IEnumerable<Cliente>> SelecionarTodos(int pagina, int tamanhoPagina);
+    Task<Cliente?> SelecionarPorId(int id);
+    Task<int> ContarTodos();
+}
