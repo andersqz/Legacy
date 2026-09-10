f178ff8c97cd1f18ac45b60beef546b8815ac4d1
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-10 08:18:59 -0300

    add vendedor dto

---
+++ Legacy.App/Dtos/VendedorDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Legacy.App.Dtos;
+
+public class VendedorDto
+{
+    public int VendedorId { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public char Tipo { get; set; }
+    public bool VendedorAtivo { get; set; }
+    public string CpfCnpj { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+}
