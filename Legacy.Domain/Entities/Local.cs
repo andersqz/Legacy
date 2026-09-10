e3fc6ff88518ba9060e447980f11580f7a958e00
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:16:12 -0300

    add local entity

---
+++ Legacy.Domain/Entities/Local.cs
@@ -0,0 +1,21 @@
+﻿namespace Legacy.Domain.Entities;
+
+public class Local
+{
+    public int EmpresaId { get; set; }
+    public int LocalId { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public string CNPJ { get; set; } = string.Empty;
+    public string InscricaoEstadual { get; set; } = string.Empty;
+    public string Endereco { get; set; } = string.Empty;
+    public string Bairro { get; set; } = string.Empty;
+    public string CEP { get; set; } = string.Empty;
+    public string UF { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string Email { get; set; }  = string.Empty;
+
+    public Local()
+    {
+        
+    }
+}
