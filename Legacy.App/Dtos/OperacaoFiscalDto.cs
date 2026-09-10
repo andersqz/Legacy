a476aad86490c4afc2de1135fc426f71a5853135
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-10 08:10:08 -0300

    add operacao fiscal dto

---
+++ Legacy.App/Dtos/OperacaoFiscalDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Legacy.App.Dtos;
+
+public class OperacaoFiscalDto
+{
+    public int EmpresaId { get; set; }
+    public int OperacaoId { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+
+
+    public OperacaoFiscalDto(int empresa, int op, string desc)
+    {
+        EmpresaId = empresa;
+        OperacaoId = op;
+        Descricao = desc;
+    }
+}
