782255bb24658a8942a095f8bc2dc9aac0d17194
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:19:39 -0300

    add transportadora entity

---
+++ Legacy.Domain/Entities/Transportadora.cs
@@ -0,0 +1,15 @@
+﻿namespace Legacy.Domain.Entities;
+
+public class Transportadora
+{
+    public int EmpresaId { get; set; }
+    public int TransportadoraId { get; set; }
+    public string Descricao { get; set; }
+    public string Endereco { get; set; }
+    public string CEP { get; set; }
+    public char TipoPessoa { get; set; }    
+    public string CpfCnpj { get; set; }
+    public string InscricaoEstadual { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
