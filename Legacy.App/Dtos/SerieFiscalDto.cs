4d6a18f0312326f85610fee519661dc6e8de0b47
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-10 08:15:04 -0300

    add serie fiscal dto

---
+++ Legacy.App/Dtos/SerieFiscalDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Legacy.App.Dtos;
+
+public class SerieFiscalDto
+{
+    public int EmpresaId { get; set; } // empresa
+    public int LocalId { get; set; } //ser001
+    public string Serie { get; set; } = string.Empty; // ser002
+    public DateOnly? UltimoDocEmitido { get; set; } // ser004 int date time
+}
