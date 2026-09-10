2ac58d99b07097a2fc18b2af840b036eddcb1714
andersqz <anderson.carvalho@ufn.edu.br>
2026-09-09 21:32:09 -0300

    add empresa interface-repository

---
+++ Legacy.Domain/Interfaces/IEmpresaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Legacy.Domain.Entities;
+
+namespace Legacy.Domain.Interfaces;
+
+public interface IEmpresaRepository
+{
+    Task<IEnumerable<Empresa>> SelecionarTodos();
+    Task<Empresa?> SelecionarPorId(int id);
+}
